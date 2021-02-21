9681e8005d1adb6e2c92ffecb3bdcfc9712619f8
rufex <rufex.dev@gmail.com>
2021-02-21 19:04:58 +0100

    CLI argument to select which invoice to export

--- gen_invoice.py
+++ gen_invoice.py
@@ -1,12 +1,20 @@
+import sys
+
 from src.config import root_path, config_yaml, invoice_dictio
 from src.components import seller_info, generate_pdf, output_name_invoice_number
-from src.spreadsheet import get_last_invoice_details
+from src.spreadsheet import get_invoice_details
+
+# CLI arguments
+if len(sys.argv) > 1:
+    cli_arg_invoice_nr = sys.argv[1]
+else:
+    cli_arg_invoice_nr = None
 
 # Load Seller Information from YAML file
 seller_info(config_yaml['SellerInfo']['Name'],config_yaml['SellerInfo']['Address'],config_yaml['SellerInfo']['City'],config_yaml['SellerInfo']['VAT'],config_yaml['SellerInfo']['IBAN'])
    
 # Invoice data from Spreadsheet
-get_last_invoice_details()
+get_invoice_details(cli_arg_invoice_nr)
 
 # Use the invoice number as the PDF export name
 invoice_nr_path = output_name_invoice_number()
--- src/spreadsheet.py
+++ src/spreadsheet.py
@@ -5,17 +5,40 @@ from .config import root_path, invoice_dictio
 from .components import client_info, invoice_info, add_product
 
 # Paths
-spreadheet_path = root_path.joinpath('spreadsheet', 'template.xlsx')
+spreadsheet_path = root_path.joinpath('spreadsheet', 'template.xlsx')
 
 # Pandas DataFrame with all the invoices
-invoices_df = pd.read_excel(spreadheet_path, sheet_name='Invoices')
+invoices_df = pd.read_excel(spreadsheet_path, sheet_name='Invoices')
+# Rename DF columns names (position in the spreadsheet is important!)
+invoices_df = invoices_df.rename(columns={
+    invoices_df.columns[0]: 'invoice_number', 
+    invoices_df.columns[1]: 'invoice_date', 
+    invoices_df.columns[2]: 'client_name', 
+    invoices_df.columns[3]: 'client_address', 
+    invoices_df.columns[4]: 'client_city', 
+    invoices_df.columns[5]: 'client_vat', 
+    invoices_df.columns[6]: 'product_description', 
+    invoices_df.columns[7]: 'product_units_amount', 
+    invoices_df.columns[8]: 'product_unit_price'
+    })
 
-def get_last_invoice_details():
-    """Get the last invoice in the spreadsheet."""
-    last_row = invoices_df.iloc[-1]
+def get_invoice_details(invoice_nr = None):
+    """Get the specific invoice in the spreadsheet.
+    If no specific invoice nr is passed, the last one is retrieved.
+    """
+    if invoice_nr != None:
+        try:
+            invoice_index = invoices_df.index[invoices_df['invoice_number'] == invoice_nr][0]
+            invoice_row = invoices_df.iloc[invoice_index]
+        except:
+            print(f'The invoice nr. "{invoice_nr}" was not founded in the spreadsheet.')
+    else:
+        # Last Row
+        invoice_row = invoices_df.iloc[-1]
+
     # Client
-    client_info(last_row['Client Name'],last_row['Client Address'],last_row['Client ZIP, City, Country'],last_row['Client VAT'])
+    client_info(invoice_row['client_name'],invoice_row['client_address'],invoice_row['client_city'],invoice_row['client_vat'])
     # Invoice Information
-    invoice_info(last_row['Invoice Date'],last_row['Invoice Number'])
+    invoice_info(invoice_row['invoice_date'],invoice_row['invoice_number'])
     # Product
-    add_product(last_row['Product Description'],last_row['Product Unit Price'],last_row['Product Units'])
+    add_product(invoice_row['product_description'],invoice_row['product_unit_price'],invoice_row['product_units_amount'])
